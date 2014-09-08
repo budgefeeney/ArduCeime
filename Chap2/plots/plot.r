8eba274f75544df28b75b52cc029051df1fef43b
Bryan Feeney <bryan.feeney@gmail.com>
2014-09-08 18:18:22 +0100

    Added plots with results to writeup

---
+++ Chap2/plots/plot.r
@@ -0,0 +1,29 @@
+library("ggplot2")
+library("gridExtra")
+
+# Load and parse file into factors and measures
+expr <- read.csv("/Users/bryanfeeney/Workspace/ArduCeime/Chap2/plots/Tweet750Results.csv")
+expr$K = factor(expr$K)
+expr$P = factor(expr$P)
+
+qplot(data=subset(expr, algor=='STM/Bouchard' | algor=='STM/Bohning'), y=QueryPerplexity, x=K, color=dataset, shape=dataset, facets=algor~P)
+#qplot(data=subset(expr, dataset=='Tweet750' & (algor=='LDA' | algor=='CTM/Bohning' | algor=='CTM/Bouchard')), y=QueryPerplexity, x=K, color=algor, shape=algor, facets=~dataset)
+      
+l <- qplot(data=subset(expr, dataset=='Tweet750' & (algor=='LDA' | algor=='CTM/Bohning' | algor=='CTM/Bouchard')), y=QueryPerplexity, x=K, color=algor, shape=algor, facets=~dataset)
+r <- qplot(data=subset(expr, dataset=='Tweet750' & (algor=='LDA' | algor=='CTM/Bohning' | algor=='CTM/Bouchard')), y=TrainPerplexity, x=K, color=algor, shape=algor, facets=~dataset)
+
+g_legend<-function(p){
+  tmp <- ggplotGrob(p)
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)}
+
+legend <- g_legend(l)
+lwidth <- sum(legend$width)
+
+## using grid.arrange for convenience
+## could also manually push viewports
+grid.arrange(arrangeGrob(l + theme(legend.position="none"),
+                         r + theme(legend.position="none")
+                        ), legend, 
+             widths=unit.c(unit(1, "npc") - lwidth, lwidth), nrow=1)
