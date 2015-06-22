0c8eb3cadc9e284480a4926d508faec4b5c6c565
Bryan Feeney <bryan.feeney@gmail.com>
2015-06-22 12:28:22 +0100

    Updated the build script

--- build.sh
+++ build.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 read -d '' chap1Files <<EOF
-Chap1/MTL-Intro.tex	
-Chap1/MTL-Kro.tex
-Chap1/MTL-GaussScale.tex
-Chap1/LangModels.tex
-Chap1/Admix-Intro.tex
-Chap1/Admix-DPs.tex		
-Chap1/Admix-Inference.tex	
-Chap1/Admix-Covariates.tex	
-Chap1/Admix-Eval.tex		
-Chap1/Admix-OtherLangModels.tex	
+Chap1/BayesianInference.tex	
 Chap1/NonConjVariational.tex
+EOF
+
+read -d '' chap2Files <<EOF
+Chap2/LangModels.tex
+Chap2/Admix-Intro.tex
+Chap2/Admix-DPs.tex
+Chap2/Admix-OtherLangModels.tex
+Chap2/Admix-Eval.tex
+Chap2/Admix-Inference.tex
+Chap2/Admix-OtherModelLinks.tex
+EOF
+
+read -d '' chap3Files <<EOF
+Chap3/Admix-Covariates.tex
+Chap3/MTL-Intro.tex
+Chap3/main.tex
 EOF
 
 
@@ -28,14 +35,23 @@ echo "\\maketitle" >> $outfile
 echo "\\tableofcontents" >> $outfile
 echo "\\clearpage" >> $outfile
 
-echo "\\section{Literature Review}" >> $outfile
+#echo "\\section{Literature Review}" >> $outfile
 
 for chap1File in $chap1Files;
 do
 	echo $chap1File
 	tail -n +2 $chap1File | sed -e "s/\\input{..\/footer.tex}//g" | sed -e "s/plots\//Chap1\/plots\//g" >> $outfile
 done
-tail -n +2 "Chap2/main.tex" | sed -e "s/\\input{..\/footer.tex}//g" | sed -e "s/plots\//Chap2\/plots\//g" >> $outfile
+for chap2File in $chap2Files;
+do
+	echo $chap2File
+	tail -n +2 $chap2File | sed -e "s/\\input{..\/footer.tex}//g" | sed -e "s/plots\//Chap2\/plots\//g" >> $outfile
+done
+for chap3File in $chap3Files;
+do
+	echo $chap3File
+	tail -n +2 $chap3File | sed -e "s/\\input{..\/footer.tex}//g" | sed -e "s/plots\//Chap3\/plots\//g" >> $outfile
+done
 
 echo "\input{footer.tex}" >> $outfile
 echo >> $outfile
