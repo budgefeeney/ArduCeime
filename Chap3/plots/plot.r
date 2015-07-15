8e501ed50ee89eb3fa939fd4f1484191716714f2
Bryan Feeney <bryan.feeney@gmail.com>
2015-07-15 16:47:24 +0100

    Changed script to use latest results.

--- Chap3/plots/plot.r
+++ Chap3/plots/plot.r
@@ -2,15 +2,15 @@ library("ggplot2")
 library("gridExtra")
 
 # Load and parse file into factors and measures
-expr <- read.csv("/Users/bryanfeeney/Workspace/ArduCeime/Chap2/plots/Tweet750Results.csv")
+expr <- read.csv("/Users/bryanfeeney/iCloud/Results/SideTopicResults/results-3.csv")
 expr$K = factor(expr$K)
 expr$P = factor(expr$P)
 
-qplot(data=subset(expr, algor=='STM/Bouchard' | algor=='STM/Bohning'), y=TrainPerplexity, x=K, shape=dataset, facets=algor~P) + ylab('Train Perplexity') + xlab('Topic Count') + labs(shape='Features') + theme_bw()
+qplot(data=subset(expr, Algor=='STM_bouchard' | Algor=='STM_bohning'), y=Tperp, x=K, shape=Dataset, facets=Algor~P) + ylab('Train Perplexity') + xlab('Topic Count') + labs(shape='Features') + theme_bw()
 #qplot(data=subset(expr, dataset=='Tweet750' & (algor=='LDA' | algor=='CTM/Bohning' | algor=='CTM/Bouchard')), y=QueryPerplexity, x=K, color=algor, shape=algor, facets=~dataset)
       
-l <- qplot(data=subset(expr, dataset=='Tweet750' & (algor=='LDA' | algor=='CTM/Bohning' | algor=='CTM/Bouchard')), y=QueryPerplexity, x=K, shape=algor, facets=~dataset) + ylab('Query Perplexit') + xlab('Topic Count') + theme_bw()
-r <- qplot(data=subset(expr, dataset=='Tweet750' & (algor=='LDA' | algor=='CTM/Bohning' | algor=='CTM/Bouchard')), y=TrainPerplexity, x=K, shape=algor, facets=~dataset) + ylab('Query Perplexit') + xlab('Topic Count') + theme_bw()
+l <- qplot(data=subset(expr, Dataset=='Tweets' & (Algor=='LDA/VB' | Algor=='CTM_bohning' | Algor=='CTM_bouchard')), y=Qperp, x=K, shape=Algor, facets=~Dataset) + ylab('Query Perplexity') + xlab('Topic Count') + theme_bw()
+r <- qplot(data=subset(expr, Dataset=='Tweets' & (Algor=='LDA/VB' | Algor=='CTM_bohning' | Algor=='CTM_bouchard')), y=Tperp, x=K, shape=Algor, facets=~Dataset) + ylab('Train Perplexity') + xlab('Topic Count') + theme_bw()
 
 g_legend<-function(p){
   tmp <- ggplotGrob(p)
